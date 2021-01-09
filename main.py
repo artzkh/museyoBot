40281cb151d84d593940b94c403f0622f0012a83
Artyom Zaharov <arrmf1@mail.ru>
2021-01-10 01:30:08 +0700

    Hello World Bot

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-# Test commit
+import os
+from dotenv import load_dotenv
+from vkbottle.bot import Bot
+
+load_dotenv()
+
+BOT_TOKEN = str(os.getenv("BOT_TOKEN"))
+
+bot = Bot(BOT_TOKEN)
+
+
+@bot.on.message()
+async def handler(_) -> str:
+    return "Hello world!"
+
+
+bot.run_forever()
